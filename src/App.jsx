69fa527cca59025c9764ca32e4272c2bbe3582fa
Sean <soxfansean@gmail.com>
2023-08-05 17:33:57 -0400

    Create Nav component with links to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Nav from "./components/Nav";
 
 function App() {
 
   return (
     <>
       <h1>This is the app!</h1>
+      <Nav />
       <Outlet />
     </>
   );
