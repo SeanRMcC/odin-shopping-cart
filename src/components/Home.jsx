644fc75993398cdad9d2fa386b73c6b0a5a95b49
Sean <soxfansean@gmail.com>
2023-08-06 14:25:42 -0400

    Create basic home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,14 @@
+import bunnyImage from "./../assets/bunny.jpeg";
+
 function Home(){
     return (
-        <p>This is the home!</p>
+        <>
+            <h1>Welcome to Sean's store!</h1>
+            <p>We sell products at the lowest prices and the highest qualitiites! Our products range from clothing, to jewelry, to weapons of mass destruction!</p>
+            <h2>Meet the man behind the store:</h2>
+            <img src={bunnyImage} alt="Sean McCarty" />
+            <p>This is Sean, a 19 year old computer science study who sells goods at criminally low prices! His secret: theft! Sean and his team steal the items on this store from warehouses across the world, allowing 100% profits and low prices for everyone! It truly is a win-win!</p>
+        </>
     );
 }
 
